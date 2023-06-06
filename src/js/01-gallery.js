1074ec77933ace76ae3772567dce6292ac939096
VladislavGuliy <vladgulij777@gmail.com>
2023-06-06 19:40:08 +0300

    Completion of task number 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const listGalleryEl = document.querySelector('.gallery');
+
+const elements = createGalleryImages(galleryItems);
+
+listGalleryEl.insertAdjacentHTML('beforeend', elements);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+function createGalleryImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
